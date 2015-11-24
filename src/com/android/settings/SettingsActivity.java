ee6f17cb05909491dc67a2ec597ac9b99b30d1fa
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-24 06:50:12 -0500

    settings: Only show mobile_networks if we have telephony
    Change-Id: I937eaca7abefb4354cc274d5d5934a33149f4414

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1258,6 +1258,10 @@ public class SettingsActivity extends Activity
                     if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
                         removeTile = true;
                     }
+                 } else if (id == R.id.mobile_networks) {
+                    if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+                        removeTile = true;
+                    }
                 } else if (id == R.id.data_usage_settings) {
                     // Remove data usage when kernel module not enabled
                     if (!Utils.isBandwidthControlEnabled()) {
@@ -1532,3 +1536,4 @@ public class SettingsActivity extends Activity
         mResultIntentData = resultIntentData;
     }
 }
+

dda5e9b33ec9ad2dbcc95597e5912dec98da1bc6
rc420head <dennislebhardt@yahoo.com>
2016-01-08 13:45:44 -0600

    Squash Lock Screen Weather
    Clean Up Lockscreen Weather (items & layout) [2/2]
    *remove timestamp toggle
    
    Change-Id: I3af29c61bda981b8d691b38d23292132260e7385
    by beanstown106
    
    Lockscreen weather options [2/2]
    Adapted for DU with a lot of changes to include
    
    - Created switch for extended statusbar weather
    - Mapped LockClock settings because creating a widget to just to this is stupid
    - Made use of what we had and removed redundant crap
    - Checkboxes are ugly, who's idea was that? Also "weather informations" hahaha
    - Added dependencies because it makes sense
    - Cleaned up the code some what
    
    Change-Id: I186413598dd643ba2d7c79c248e75b2f39dfc658
    by AlmightyMegadeth00

--- src/com/android/settings/candy/ButtonSettings.java
+++ src/com/android/settings/candy/ButtonSettings.java
@@ -28,6 +28,7 @@ import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;

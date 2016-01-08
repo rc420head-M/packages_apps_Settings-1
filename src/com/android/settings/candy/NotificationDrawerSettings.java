a0940c4ab3ffa10e79e9d89dd273dd11a7b609b9
Raleigh Matlock <raleighman2@gmail.com>
2016-01-08 16:19:31 -0500

    Update QS Quick Pulldown yet again...

--- src/com/android/settings/candy/NotificationDrawerSettings.java
+++ src/com/android/settings/candy/NotificationDrawerSettings.java
@@ -13,28 +13,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.settings.candy;
-
-import android.content.ContentResolver;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceScreen;
-import android.provider.Settings;
-
-import com.android.settings.R;
-import com.android.settings.candy.qs.QSTiles;
-import com.android.settings.SettingsPreferenceFragment;
 
 import com.android.internal.logging.MetricsLogger;
 
-public class NotificationDrawerSettings extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
+import android.os.Bundle;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
 
-    private static final String QUICK_PULLDOWN = "quick_pulldown";
+import com.android.settings.candy.qs.QSTiles;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class NotificationDrawerSettings extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
+
+    private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
     private static final String QS_ORDER = "qs_order";
 
     private Preference mQSTiles;
@@ -43,8 +48,22 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.notification_drawer_settings);
 
+        addPreferencesFromResource(R.xml.notification_drawer_settings);
+        
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+    // Quick pulldown
+	mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
+        if (!Utils.isPhone(getActivity())) {
+            prefSet.removePreference(mQuickPulldown);
+        } else {
+            mQuickPulldown.setOnPreferenceChangeListener(this);
+            int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
+                    Settings.System.QS_QUICK_PULLDOWN, 0);
+            mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
+            updateQuickPulldownSummary(statusQuickPulldown);
+        }
     }
 
     @Override
@@ -55,25 +74,17 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
         mQSTiles = prefSet.findPreference(QS_ORDER);
 
         ContentResolver resolver = getActivity().getContentResolver();
-        
-        mQuickPulldown = (ListPreference) prefSet.findPreference(QUICK_PULLDOWN);
-        mQuickPulldown.setOnPreferenceChangeListener(this);
-        int quickPulldownValue = Settings.System.getIntForUser(resolver,
-                Settings.System.QS_QUICK_PULLDOWN, 0, UserHandle.USER_CURRENT);
-        mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
-        updatePulldownSummary(quickPulldownValue);
        
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getContentResolver();
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
         if (preference == mQuickPulldown) {
-            int quickPulldownValue = Integer.valueOf((String) newValue);
-            int index = mQuickPulldown.findIndexOfValue((String) newValue);
-            Settings.System.putIntForUser(resolver, Settings.System.QS_QUICK_PULLDOWN,
-                    quickPulldownValue, UserHandle.USER_CURRENT);
-            updatePulldownSummary(quickPulldownValue);
+            int statusQuickPulldown = Integer.valueOf((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_QUICK_PULLDOWN,
+                    statusQuickPulldown);
+            updateQuickPulldownSummary(statusQuickPulldown);
             return true;
         }
         return false;
@@ -81,19 +92,21 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment
     
     @Override
     protected int getMetricsCategory() {
-        return MetricsLogger.CANDY_QS;
+        return MetricsLogger.DONT_TRACK_ME_BRO;
     }
 
-    private void updatePulldownSummary(int value) {
+    private void updateQuickPulldownSummary(int value) {
         Resources res = getResources();
 
         if (value == 0) {
             // quick pulldown deactivated
             mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
         } else {
+            Locale l = Locale.getDefault();
+            boolean isRtl = TextUtils.getLayoutDirectionFromLocale(l) == View.LAYOUT_DIRECTION_RTL;
             String direction = res.getString(value == 2
-                    ? R.string.quick_pulldown_summary_left
-                    : R.string.quick_pulldown_summary_right);
+                    ? (isRtl ? R.string.quick_pulldown_summary_right : R.string.quick_pulldown_summary_left)
+                    : (isRtl ? R.string.quick_pulldown_summary_left : R.string.quick_pulldown_summary_right));
             mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary, direction));
         }
     }

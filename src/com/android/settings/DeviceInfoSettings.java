efde69f99377cb9189bf1b1f85791947e955812e
rc420head <dennislebhardt@yahoo.com>
2016-03-01 21:57:15 -0600

    Turn The Oven On We're Making Candy

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -82,6 +82,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_MOD_BUILD_DATE = "build_date";
+    private static final String KEY_CANDY_VERSION = "candy_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -129,6 +131,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        setValueSummary(KEY_CANDY_VERSION, "ro.candy.version");
+        findPreference(KEY_CANDY_VERSION).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
